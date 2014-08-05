851b75eea90c2188ac637fc9942f212913f69b50
mikolas <mikolas@gmail.com>
2014-08-05 20:55:10 +0100

    Adding a small script  for testing.

---
+++ src/sort/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# File:  test.sh
+# Author:  mikolas
+# Created on:  Tue Aug 5 20:46:39 WEST 2014
+# Copyright (C) 2014, Mikolas Janota
+#
+for f in ./examples/err*.qcir; do
+  ./qcir_sort $f
+  if [ $? -eq 0 ]; then
+    echo '===============FAIL'
+    exit 100
+  fi
+done
+for f in ./examples/t*.qcir; do
+  ./qcir_sort $f
+  if [ $? -ne 0 ]; then
+    echo '===============FAIL'
+    exit 100
+  fi
+done
+echo '===============OK'
+exit 0
